377e3fa33f017907f6b5c70d32cb754a7ec26fd0
DouglasGo8 <sgtear@hotmail.com>
2021-01-16 18:03:15 -0300

    Fanout done..

---
+++ direct-exchange/src/main/java/com/udemy/rabbitmq/integrations/direct/exchange/DirectExchangeApplication.java
@@ -0,0 +1,12 @@
+package com.udemy.rabbitmq.integrations.direct.exchange;
+
+/**
+ * @author dbatista
+ */
+@SpringApplication
+public class DirectExchangeApplication {
+    
+    public static void main(String... args) {
+        SpringApplication.run(DirectExchangeApplication.class, args);
+    }
+}
---
+++ direct-exchange/src/main/java/com/udemy/rabbitmq/integrations/direct/exchange/model/Image.java
@@ -0,0 +1,8 @@
+package com.udemy.rabbitmq.integrations.direct.exchange.model;
+
+/**
+ * @author dbatista
+ */
+public class Image {
+    
+}
